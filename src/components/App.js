3482d5acfa75c370339186b4f9584826c5438ad9
andnane adjana <andnaneadjana@yahoo.fr>
2021-03-22 11:14:48 -0400

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos'
+
+
+
+const App = () => { 
+	const [selectedVideo, setSelectedVideo] = useState(null)
+	const [videos, search] = useVideos("nature")
+
+	useEffect(() => {
+		setSelectedVideo(videos[0]) // select the first video on the list
+	}, [videos]) 
+	
+
+
+	return (
+		<div className="ui container">
+		  <SearchBar onFormSubmit={search} />
+		  <div className="ui grid">
+			<div className="ui row">
+			  <div className="eleven wide column">
+				<VideoDetail video={selectedVideo} />
+			  </div>
+			  <div className="five wide column">
+				<VideoList
+				  onVideoSelect={(video) => setSelectedVideo(video)}
+				  videos={ videos}
+				/>
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  );
+
+}
+
+
+export default App;
